638a014d486f2c50f070eb46efd31efc62a2f500
Lucianna Tittle <lucianna.roset@gmail.com>
2021-03-26 18:56:26 -0500

    Reorganized routes.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
+import { Route, Router } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <Router>
+      <Route path="/" exact component={Home} />
+    </Router>
   );
 };
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = (props) => {
+  return (
+    <>
+      <h1>Lambda Eats</h1>
+      <p>You can remove this code and create your own header</p>
+    </>
+  );
+};
+
+export default Home;
