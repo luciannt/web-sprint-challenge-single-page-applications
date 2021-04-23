42a9a7e5b535e6ca8ad122a908e396d5cce76134
Lucianna Tittle <lucianna.roset@gmail.com>
2021-04-23 16:17:33 -0500

    Fixing project to start new Sprint Challenge.

--- src/components/PizzaForm.jsx
+++ src/components/PizzaForm.jsx
@@ -1,9 +1,50 @@
 import React from "react";
+import PizzaCover from "../Assets/pizza-order-cover.png";
 
 const PizzaForm = (props) => {
   return (
     <form>
-      <input placeholder="Order Name" />
+      <div className="main">
+        <h2>Famous Brooklyn Pizza</h2>
+        <input name="name" placeholder="Order Name" />
+        <select>
+          <option>Pizza Size</option>
+          <option>Small</option>
+          <option>Medium</option>
+          <option>Large</option>
+          <option>Extra Large</option>
+        </select>
+        <div className="toppings">
+          <span>
+            Pepperoni
+            <input type="checkbox" value="Pepperoni" name="Pepperoni" />
+          </span>
+          <span>
+            Sausage
+            <input type="checkbox" value="Sausage" name="Sausage" />
+          </span>
+          <span>
+            Anchovies
+            <input type="checkbox" value="Anchovies" name="Anchovies" />
+          </span>
+          <span>
+            Extra Cheese
+            <input type="checkbox" value="Extra Cheese" name="Extra Cheese" />
+          </span>
+          <span>
+            Bell Peppers
+            <input type="checkbox" value="Bell Peppers" name="Bell Peppers" />
+          </span>
+          <span>
+            Pineapple
+            <input type="checkbox" value="Pineapple" name="Pineapple" />
+          </span>
+        </div>
+        <textarea placeholder="Special Instructions" rows="5" cols="50" />
+      </div>
+      <div className="side">
+        <img src={PizzaCover} alt="" />
+      </div>
     </form>
   );
 };
---
+++ src/components/formSchema.js
@@ -0,0 +1,13 @@
+import * as yup from "yup";
+
+const formSchema = yup.object().shape({
+  name: yup
+    .string()
+    .trim()
+    .required("Name is required")
+    .min(2, "Name must be at least two characters"),
+
+  size: yup.string().required("Please Select a size"),
+});
+
+export default formSchema;
